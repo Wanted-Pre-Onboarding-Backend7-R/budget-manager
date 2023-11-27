d7a00efb02e7a843597155d7c9e1d476cdfb9de6
Joonhee Jeong <jeonggoo75@gmail.com>
2023-11-27 15:46:40 +0900

    feat #7: 이메일로 회원 조회 dao 메소드 정의

--- src/main/java/server/preonboarding/budgetmanager/MemberRepository.java
+++ src/main/java/server/preonboarding/budgetmanager/MemberRepository.java
@@ -3,5 +3,10 @@ package server.preonboarding.budgetmanager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import server.preonboarding.budgetmanager.domain.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Optional<Member> findByEmail(String email);
+
 }
