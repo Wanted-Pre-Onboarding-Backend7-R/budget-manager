21073a6187066d5498b0ccdef59e9ce51c30b8db
Joonhee Jeong <jeonggoo75@gmail.com>
2023-11-29 23:33:27 +0900

    refactor: JwtProvider에서 JwtDecoder 분리 (#11)
    * refactor #10: JwtProvider에서 JwtDecoder 구현 분리
    
    * refactor #10: SecretKey, JwtProvider, JwtDecoder 빈 등록
    
    * test #10: 변경점에 따라 JWT 생성 후 추출
    
    의존성, 클래스이름, 표시 이름 변경

--- src/main/java/server/preonboarding/budgetmanager/security/JwtProvider.java
+++ src/main/java/server/preonboarding/budgetmanager/security/JwtProvider.java
@@ -1,27 +1,15 @@
 package server.preonboarding.budgetmanager.security;
 
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Jwts;
+import lombok.RequiredArgsConstructor;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
-@Slf4j
-@Component
+@RequiredArgsConstructor
 public class JwtProvider {
 
     private final SecretKey secretKey;
-    private final JwtParser jwtParser;
-
-    public JwtProvider(@Value("${jwt.secret}") String secret) {
-        byte[] bytes = Decoders.BASE64.decode(secret);
-        secretKey = Keys.hmacShaKeyFor(bytes);
-        jwtParser = Jwts.parser().verifyWith(secretKey).build();
-    }
 
     public String createAccessToken(Long id) {
         Date now = new Date();
@@ -30,17 +18,6 @@ public class JwtProvider {
                 .issuedAt(now)
                 .signWith(secretKey)
                 .compact();
-    }
-
-    public Long extractSubject(String accessToken) {
-        String id = getJwsClaims(accessToken)
-                .getPayload()
-                .getSubject();
-        return Long.parseLong(id);
-    }
-
-    private Jws<Claims> getJwsClaims(String accessToken) {
-        return jwtParser.parseSignedClaims(accessToken);
     }
 
 }
--- src/main/java/server/preonboarding/budgetmanager/security/SecurityConfig.java
+++ src/main/java/server/preonboarding/budgetmanager/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package server.preonboarding.budgetmanager.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -11,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import javax.crypto.SecretKey;
+
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
@@ -19,6 +24,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
             .authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(HttpMethod.GET, "/").permitAll()
                 .requestMatchers(HttpMethod.POST, "/api/members", "/api/auth/login").permitAll()
                 .anyRequest().authenticated()
             )
@@ -33,6 +39,22 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecretKey secretKey(@Value("${jwt.secret}") String secret) {
+        byte[] bytes = Decoders.BASE64.decode(secret);
+        return Keys.hmacShaKeyFor(bytes);
+    }
+
+    @Bean
+    public JwtProvider jwtProvider(SecretKey secretKey) {
+        return new JwtProvider(secretKey);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(SecretKey secretKey) {
+        return new JwtDecoder(secretKey);
     }
 
 }
